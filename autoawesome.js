407c3ec85007b8c85b21925a17d5185b39ba212e
Michael Wu <mikemwu@stanford.edu>
2011-10-18 01:05:19 -0700

    autobop

--- autoawesome.js
+++ autoawesome.js
@@ -1,21 +1,55 @@
 (function(){
-    var ttObj = null;
-    var djIterations = 0;
+  var ttObj = null;
 
-    // Get the room manager object
-    for (var prop in window) { 
-        if (window.hasOwnProperty(prop) && window[prop] instanceof roommanager){ 
-            ttObj = window[prop];
-            break;
-        } 
-    }
+  function getTurntableObjects(){
+    var roomInfo = null;
+  
+    var dfd = $.Deferred(),
+      resolveWhenReady = function() {
+      if(window.location.pathname !== "/lobby") {
+        for(var o in turntable) {
+          if(turntable[o] !== null) {
+            for(var o2 in turntable[o]) {
+              if(turntable[o][o2] !== null) {
+                if(o2 == 'creatorId') {
+                  // console.log("currentDj found in: ", o);
+                  roomInfo = turntable[o];
+                  break;
+                }
+              }
+            }
+          }
+        }
 
-    if(ttObj === null){
-        alert("Couldn't auto find the roommanger object.");
-        return;
-    }
+        if(roomInfo) {
+            for(o in roomInfo) {
+                if(roomInfo[o] !== null) {
+                    for(o2 in roomInfo[o]) {
+                        if(o2 == 'myuserid') {
+                            ttObj = roomInfo[o];
+                        }
+                    }
+                }
+            }
+            dfd.resolve();
+        } else {
+            setTimeout(function(){
+                resolveWhenReady();
+            }, 250);
+        }
+      } else {
+        setTimeout(function(){
+          resolveWhenReady();
+        }, 250);
+      }
+    };  
+    resolveWhenReady();
+
+    return dfd.promise();
+  }
  
-    // Main loop - repeat every 5 seconds
+ $.when(getTurntableObjects()).then(function() {
+      // Main loop - repeat every 5 seconds
     setInterval(function() {
         if(ttObj.current_dj) {
             if(ttObj.current_dj[0] != ttObj.myuserid) { 
@@ -23,4 +57,5 @@
             }
         }
     }, 5000);
+  });
 })();
